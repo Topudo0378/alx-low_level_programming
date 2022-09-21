017831dd83510cf1c6fdc4bca865f013c335b3b7
Topudo0378 <oferetope@gmail.com>
2022-09-21 19:04:08 +0100

    pointers arrays strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,17 +9,16 @@
  * Return: returns poiner to @dest
  * /
  
-char *_strcat(char *dest,char *src)
+char *_strcat(char *dest, char *src)
 {
 
-int index = 0,dest_len =0;
+        int index = 0,dest_len = 0;
 
-	while (dest[index++])
+        while (dest[index++])
 		dest_len++;
 	
-	for (index = 0; scr [index]; index++)
+	for (index = 0; scr[index]; index++)
 		dest[dest_len++] = src[index];
 
 	return (dest);
-
 }
